7d4443146f9288d7474913d8f0aa8fd10c5a34bb
xXValhallaCoderXx <renate.gouveia@gmail.com>
2017-10-06 02:33:31 +0800

    Small tweak

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 import { AppContainer } from 'react-hot-loader';
 import './assets/styles/main.css';
 
@@ -11,14 +12,18 @@ const store = configureStore();
 
 render(
     <AppContainer>
-        <App store={store}/>
+        <Provider store={store}>
+            <App />
+        </Provider>
     </AppContainer>, root);
 
 if (module.hot) {
     module.hot.accept('./components/App', () => {
         render(
             <AppContainer>
-                <App store={store}/>
+                <Provider store={store}>
+                    <App />
+                </Provider>
             </AppContainer>, root);
     });
 }
