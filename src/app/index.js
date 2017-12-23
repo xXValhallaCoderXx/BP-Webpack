b8e16c7cf065b7fd8d837818c4aa9d9514e871fc
xXValhallaCoderXx <renate.gouveia@gmail.com>
2017-12-23 11:41:04 +0800

    Cleaned Code

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import styles from './test.css';
+import styles from './cssModule.css';
 
 class Application extends Component {
   constructor(props) {
@@ -12,10 +12,15 @@ class Application extends Component {
   render() {
     return (
       <div>
-        <button className="global-class">GLOBAL</button>
-        <button className={styles.testClass}>TEST</button>
-        <button onClick={() => this.lazyLoad()}>LAZY LOAD</button>
-        <div>THE LAZY TEXT: {this.state.someData}</div>
+        <h3>Button Is Using A Global Style</h3>
+        <button className="global-class">Global Style</button>
+        <hr/>
+        <h3>Button using CSS Module</h3>
+        <button className={styles.testClass}>CSS Module</button>
+        <hr/>
+        <h3>Lazy Load Example</h3>
+        <button onClick={() => this.lazyLoad()}>Lazy Load Button</button>
+        <div style={{marginTop: 10}}>Dynamically Loaded: {this.state.someData}</div>
       </div>
     );
   }
