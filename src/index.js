d7bcd286dcec6d775507eb66e12b7dcc4953863d
Renate Gouveia <renate@horangi.com>
2017-12-29 10:26:57 +0800

    Added comment

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ import configureStore from "./store";
 const root = document.getElementById("render-app");
 const store = configureStore();
 
+// Currently a small warning pops up on HMR - It will be fixed shortly via the hot-reload maintainers - warning doesnt break anything
+
 render(
   <Provider store={store}>
     <App />
