a82f12fa0bfd33fb9c6083a45121bd035cd86c7e
xXValhallaMonkXx <renate.gouveia@gmail.com>
2018-03-17 23:13:08 +0800

    removed dist from git

--- src/component.js
+++ src/component.js
@@ -1,5 +1,15 @@
 export default (text = "Hello Geee") => {
   const element = document.createElement("div");
   element.innerHTML = text;
+  element.onclick = () => {
+    import("./LazyLoad")
+      .then(lazy => {
+        element.textContent = lazy.default;
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  };
+
   return element;
 };
