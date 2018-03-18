a8810cfd191366aed59ad280c6303ba89ab35bc9
xXValhallaMonkXx <renate.gouveia@gmail.com>
2018-03-19 02:41:47 +0800

    work on splits

--- src/app/app1/appOne.js
+++ src/app/app1/appOne.js
@@ -1,7 +1,4 @@
 import React, { Component } from "react";
-import { connect } from "react-redux";
-
-
 import styles from "./cssModule.scss";
 
 class Application extends Component {
---
+++ src/app/sharedCompoents/Header/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div>
+        This is a shared Component
+      </div>
+    )
+  }
+}
