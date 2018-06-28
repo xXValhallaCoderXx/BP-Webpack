c8211522dc0f3da7c0f9634c0c1aa43fab30ce2c
Renate Gouveia <renate@horangi.com>
2018-06-28 22:45:01 +0800

    Keep master branch clean

--- src/__config__/__rootReducer.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import pageOneReducer from "../app/PageOne/__PageOneDux";
-
-export default combineReducers({
-  demo: pageOneReducer
-});
--- src/app/PageThree/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const PageThree = () => {
-  return <div>PAGE THREE</div>;
-};
-
-export default PageThree;
