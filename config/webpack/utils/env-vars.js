0aa6f57de231c39a1a4963dfb3fcc41fc583669d
Renate Gouveia <renate@horangi.com>
2018-07-02 12:08:14 +0800

    Working on ENV VARs

---
+++ config/webpack/utils/env-vars.js
@@ -0,0 +1,35 @@
+const env = process.env.NODE_ENV;
+let config = require("../../env-vars.json");
+// envConfig = dev | stage | prod
+
+let envConfig = config[env];
+// Depending on the enviroment, this will pull the values from the respective .json file and add the values as process.env variables
+switch (env) {
+  case "dev":
+    Object.keys(envConfig).forEach(key => {
+      //process.env[key] = envConfig[key];
+      console.log("ENV KEY: ", envConfig[key]);
+      console.log("ENV KEY: ", env[key]);
+    });
+    break;
+  case "stage":
+    Object.keys(envConfig).forEach(key => {
+      //process.env[key] = envConfig[key];
+      console.log("ENV KEY: ", envConfig[key]);
+      console.log("ENV KEY: ", env[key]);
+    });
+    break;
+  case "prod":
+    Object.keys(envConfig).forEach(key => {
+      //process.env[key] = envConfig[key];
+      console.log("ENV KEY: ", envConfig[key]);
+      console.log("ENV KEY: ", env[key]);
+    });
+    break;
+  default:
+    Object.keys(envConfig).forEach(key => {
+      //process.env[key] = envConfig[key];
+      console.log("ENV KEY: ", envConfig[key]);
+      console.log("ENV KEY: ", env[key]);
+    });
+}
--- config/webpack/webpack.common.js
+++ config/webpack/webpack.common.js
@@ -2,11 +2,35 @@ const merge = require("webpack-merge");
 const parts = require("./utils/webpack.parts");
 const webpack = require("webpack");
 
+//const envVars = require("./utils/env-vars");
+
+// const env = "dev";
+// let config = require("../env-vars.json");
+// //console.log("PROCESS: ", process.env.NODE_ENV)
+// //envConfig = dev | stage | prod
+
+// // WORKING ON ENV VARIBL ELOADING FROM JSON
+
+// let envConfig = config[env];
+// let dataObject = []
+// console.log("ENV CONFIG: ", envConfig);
+
+
+// console.log("LALALAL: ", dataObject)
+
+// Object.keys(envConfig).forEach(key => {
+   
+//   parts.setFreeVariable(key, envConfig[key]),
+// });
 const commonConfig = merge([
   {
     plugins: [new webpack.NamedModulesPlugin()]
   },
-  parts.setFreeVariable("SOME_VAR", "This is from the freeee variables")
+  // Object.keys(envConfig).forEach(key => {
+  //     console.log("WHAT IS THIS : ", envConfig[key])
+  //   //parts.setFreeVariable(key, envConfig[key]),
+  // });
+  
 ]);
 
 module.exports = commonConfig;
